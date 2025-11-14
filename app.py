c1e1e01d61fafdf4dcbb5dc84d03bf711867a6ff
AlanCabezas <aac.cabezas@yavirac.edu.ec>
2025-11-14 14:40:52 -0500

    segundo

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "¡Hola Mundo desde Flask con Traefik! 🚀"
+def home():
+    return "<h1>Hola desde Flask con Traefik 🚀</h1>"
+
+@app.route('/saludo/<nombre>')
+def saludo(nombre):
+    return f"<h2>Hola {nombre}, bienvenido a pgmoreno.byronrm.com</h2>"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
